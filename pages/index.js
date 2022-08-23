72689386dc70075a8ef79bc3309fc3c8fdff93e3
Vokuc <93860771+Vokuc@users.noreply.github.com>
2022-08-23 12:35:44 +0100

    updates to home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -9,7 +10,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          Hi There, Welcome to my website, My name is <span>Ukpai Chika Kalu</span> I am also known as <span>Vokuc, </span>
+          I am a music producer and software engineer What aspect of my work are you interested in?
+          Want to check out my <Link href="/pages/music">music</Link>
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
