58ad9f8404855196b51029aa6df31bddf3410588
Vokuc <93860771+Vokuc@users.noreply.github.com>
2022-08-23 12:57:43 +0100

    update to index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hi There, Welcome to my website, My name is <span>Ukpai Chika Kalu</span> I am also known as <span>Vokuc, </span>
-          I am a music producer and software engineer What aspect of my work are you interested in?
-          Want to check out my <Link href="/pages/music">music</Link>
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>
+					Hi There, Welcome to my website, My name is{" "}
+					<span className="text-white">Ukpai Chika Kalu</span> I am
+					also known as
+					<span className="text-white"> Vokuc</span>, I am a music
+					producer and software engineer. Feel free to check out my
+					music<Link href="/pages/music">here</Link>, or some of the
+					software engineering projects I am working on{" "}
+					<Link href="/pages/software-engineering">here</Link>
+				</p>
+			</section>
+		</Layout>
+	);
 }
